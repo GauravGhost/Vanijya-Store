9ba78e358abb4b81a3838ba377415789212c03d5
gauravghost <gyanendrak874@gmail.com>
2022-12-23 18:45:48 +0530

    first commit added basic structure and made navbar also added hero section

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+import HeroSection from './components/HeroSection'
+const Home = () => {
+  const data = {
+    name: "Vanijya Store"
+  }
+  return (
+    <HeroSection myData={data}/>
+  )
+}
+
+export default Home
