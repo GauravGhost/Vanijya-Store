8b5d0465f0ea861a710a2af7789250a840880e35
gauravghost <gyanendrak874@gmail.com>
2022-12-23 23:47:35 +0530

    added Services and trusted page in home section

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import styled from 'styled-components'
+
 import HeroSection from './components/HeroSection'
+import Services from './components/Services'
+import Trusted from './components/Trusted'
 const Home = () => {
   const data = {
     name: "Vanijya Store"
   }
-  return (
-    <HeroSection myData={data}/>
-  )
+  return <>
+    <HeroSection myData={data} />
+    <Services />
+    <Trusted />
+  </>
+
 }
 
 export default Home
